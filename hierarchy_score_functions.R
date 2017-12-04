9b6141e4b1c9089c9e1f76d32ade9a0873d22f80
Kaushik Mohan <kaushik.s.mohan@gmail.com>
2017-12-03 23:54:52 -0500

    final scaping code with data cleaning

--- hierarchy_score_functions.R
+++ hierarchy_score_functions.R
@@ -31,37 +31,3 @@ get_hierarchy_score_bw <- function(g){
   return(hierarchy_matrix)
 }
 
-
-## Page-Rank based Hierarchy score
-get_hierarchy_score_pr <- function(g){
-  
-}
-  
-## HITS based Hierarchy score
-get_hierarchy_score_hits <- function(g){
-  
-}
-
-## Attraction Basin based Hierarchy score
-get_hierarchy_score_ab <- function(g,alpha=2){
-  
-}
-
-
-get_tree_from_hierarchy_score <- function(h,upper_cutoff=0.8,lower_cutoff=0.1){
-  upper_cutoff <- upper_cutoff
-  lower_cutoff <- lower_cutoff
-  h[h > upper_cutoff] <- NA
-  h[h < lower_cutoff] <- NA  
-  
-  ## Get Tree Structure
-  
-}
-  
-
-
-
-
-
-
-
